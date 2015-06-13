28dfa1226b446fab200c86eddf0fccc21b2461e2
liushuang <liushuang@163.com>
2015-06-13 10:16:57 +0800

    edit hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 ﻿public class Hello {
 	
 	public static void main(String [] args) {
-		System.out.println("second change! in brh branch");
-		System.out.println("test git! in brh branch");
+		System.out.println("second change! in master branch");
+		System.out.println("test git! in master branch");
 	}
 }
